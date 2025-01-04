0491d0b100c0c4866329d5e4865a41d12e9025a5
lilin <7189334@qq.com>
2025-01-04 22:29:14 +0800

    添加sln工程文件，方便debug

--- README.txt
+++ README.txt
@@ -25,6 +25,12 @@ More information about the scripts is provided at https://code.google.com/p/word
 有关脚本的更多信息，请访问 https://code.google.com/p/word2vec/
 
 -------------------------------------------------------------------------------------
+win11下vs2022的.sln工程文件的生成：
+mkdir build
+cd build
+cmake ..
+
+-------------------------------------------------------------------------------------
 ../demo-word.sh 文件中有测试数据的下载地址
 http://mattmahoney.net/dc/text8.zip
 解压后得到text8文件。
@@ -39,3 +45,75 @@ text8就放在当前目录，避免修改脚本。
 
 -------------------------------------------------------------------------------------
 ./word-analogy.exe vectors.bin
+
+
+
+-------------------------------------------------------------------------------------
+./word2vec.exe -train text8 -output vectors.bin
+               -cbow 1      使用连续词袋模型cbow；默认值为 1（对于 skip-gram 模型使用 0）
+               -size 200    词向量大小(维度)
+               -window 8    词之间的最大跳过长度
+               -negative 25 负面示例的数量
+               -hs 0        使用分层 Softmax；默认值为 0（未使用）
+               -sample 1e-4 词出现的阈值
+               -threads 20 
+               -binary 1    以二进制模式保存结果向量；默认值为 0（关闭）
+               -iter 15     训练迭代
+
+
+PS C:\code\word2vec> ./word2vec.exe
+WORD VECTOR estimation toolkit v 0.1c
+
+Options:
+Parameters for training:
+        -train <file>
+                Use text data from <file> to train the model
+        -output <file>
+                Use <file> to save the resulting word vectors / word clusters
+        -size <int>
+                Set size of word vectors; default is 100
+        -window <int>
+                Set max skip length between words; default is 5
+        -sample <float>
+                Set threshold for occurrence of words. Those that appear with higher frequency in the training data
+                will be randomly down-sampled; default is 1e-3, useful range is (0, 1e-5)
+        -hs <int>
+                Use Hierarchical Softmax; default is 0 (not used)
+        -negative <int>
+                Number of negative examples; default is 5, common values are 3 - 10 (0 = not used)
+        -threads <int>
+                Use <int> threads (default 12)
+        -iter <int>
+                Run more training iterations (default 5)
+        -min-count <int>
+                This will discard words that appear less than <int> times; default is 5
+        -alpha <float>
+                Set the starting learning rate; default is 0.025 for skip-gram and 0.05 for CBOW
+        -classes <int>
+                Output word classes rather than word vectors; default number of classes is 0 (vectors are written)
+        -debug <int>
+                Set the debug mode (default = 2 = more info during training)
+        -binary <int>
+                Save the resulting vectors in binary moded; default is 0 (off)
+        -save-vocab <file>
+                The vocabulary will be saved to <file>
+        -read-vocab <file>
+                The vocabulary will be read from <file>, not constructed from the training data
+        -cbow <int>
+                Use the continuous bag of words model; default is 1 (use 0 for skip-gram model)
+
+Examples:
+./word2vec -train data.txt -output vec.txt -size 200 -window 5 -sample 1e-4 -negative 5 -hs 0 -binary 0 -cbow 1 -iter 3
+-------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
--- distance.c
+++ distance.c
@@ -18,130 +18,162 @@
 #include <stdlib.h>
 
 #ifdef _WIN32
-#define max_size ((long long)2000)          // max length of strings
-#define N  ((long long)40)                  // number of closest words that will be shown
-#define max_w  ((long long)50)              // max length of vocabulary entries
+#define max_size ((long long)2000)       // max length of strings
+#define N  ((long long)40)               // number of closest words that will be shown，输出时展示与输入单词余弦相似度最高的前 N 个单词
+#define max_w  ((long long)50)           // max length of vocabulary entries，词汇表中单词的最大长度
 #else
 const long long max_size = 2000;         // max length of strings
 const long long N = 40;                  // number of closest words that will be shown
 const long long max_w = 50;              // max length of vocabulary entries
 #endif
 
-int main(int argc, char **argv) {
-  FILE *f;
-  char st1[max_size];
-  char *bestw[N];
-  char file_name[max_size], st[100][max_size];
-  float dist, len, bestd[N], vec[max_size];
-  long long words, size, a, b, c, d, cn, bi[100];
-  float *M;
-  char *vocab;
-  if (argc < 2) {
-    printf("Usage: ./distance <FILE>\nwhere FILE contains word projections in the BINARY FORMAT\n");
-    return 0;
-  }
-  strcpy(file_name, argv[1]);
-  f = fopen(file_name, "rb");
-  if (f == NULL) {
-    printf("Input file not found\n");
-    return -1;
-  }
-  fscanf(f, "%lld", &words);
-  fscanf(f, "%lld", &size);
-  vocab = (char *)malloc((long long)words * max_w * sizeof(char));
-  for (a = 0; a < N; a++) bestw[a] = (char *)malloc(max_size * sizeof(char));
-  M = (float *)malloc((long long)words * (long long)size * sizeof(float));
-  if (M == NULL) {
-    printf("Cannot allocate memory: %lld MB    %lld  %lld\n", (long long)words * size * sizeof(float) / 1048576, words, size);
-    return -1;
-  }
-  for (b = 0; b < words; b++) {
-    a = 0;
-    while (1) {
-      vocab[b * max_w + a] = fgetc(f);
-      if (feof(f) || (vocab[b * max_w + a] == ' ')) break;
-      if ((a < max_w) && (vocab[b * max_w + a] != '\n')) a++;
-    }
-    vocab[b * max_w + a] = 0;
-    for (a = 0; a < size; a++) fread(&M[a + b * size], sizeof(float), 1, f);
-    len = 0;
-    for (a = 0; a < size; a++) len += M[a + b * size] * M[a + b * size];
-    len = sqrt(len);
-    for (a = 0; a < size; a++) M[a + b * size] /= len;
-  }
-  fclose(f);
-  while (1) {
-    for (a = 0; a < N; a++) bestd[a] = 0;
-    for (a = 0; a < N; a++) bestw[a][0] = 0;
-    printf("Enter word or sentence (EXIT to break): ");
-    a = 0;
-    while (1) {
-      st1[a] = fgetc(stdin);
-      if ((st1[a] == '\n') || (a >= max_size - 1)) {
-        st1[a] = 0;
-        break;
-      }
-      a++;
-    }
-    if (!strcmp(st1, "EXIT")) break;
-    cn = 0;
-    b = 0;
-    c = 0;
-    while (1) {
-      st[cn][b] = st1[c];
-      b++;
-      c++;
-      st[cn][b] = 0;
-      if (st1[c] == 0) break;
-      if (st1[c] == ' ') {
-        cn++;
-        b = 0;
-        c++;
-      }
-    }
-    cn++;
-    for (a = 0; a < cn; a++) {
-      for (b = 0; b < words; b++) if (!strcmp(&vocab[b * max_w], st[a])) break;
-      if (b == words) b = -1;
-      bi[a] = b;
-      printf("\nWord: %s  Position in vocabulary: %lld\n", st[a], bi[a]);
-      if (b == -1) {
-        printf("Out of dictionary word!\n");
-        break;
-      }
-    }
-    if (b == -1) continue;
-    printf("\n                                              Word       Cosine distance\n------------------------------------------------------------------------\n");
-    for (a = 0; a < size; a++) vec[a] = 0;
-    for (b = 0; b < cn; b++) {
-      if (bi[b] == -1) continue;
-      for (a = 0; a < size; a++) vec[a] += M[a + bi[b] * size];
-    }
-    len = 0;
-    for (a = 0; a < size; a++) len += vec[a] * vec[a];
-    len = sqrt(len);
-    for (a = 0; a < size; a++) vec[a] /= len;
-    for (a = 0; a < N; a++) bestd[a] = -1;
-    for (a = 0; a < N; a++) bestw[a][0] = 0;
-    for (c = 0; c < words; c++) {
-      a = 0;
-      for (b = 0; b < cn; b++) if (bi[b] == c) a = 1;
-      if (a == 1) continue;
-      dist = 0;
-      for (a = 0; a < size; a++) dist += vec[a] * M[a + c * size];
-      for (a = 0; a < N; a++) {
-        if (dist > bestd[a]) {
-          for (d = N - 1; d > a; d--) {
-            bestd[d] = bestd[d - 1];
-            strcpy(bestw[d], bestw[d - 1]);
-          }
-          bestd[a] = dist;
-          strcpy(bestw[a], &vocab[c * max_w]);
-          break;
-        }
-      }
-    }
-    for (a = 0; a < N; a++) printf("%50s\t\t%f\n", bestw[a], bestd[a]);
-  }
-  return 0;
+void print_word_vec(char* word_str, float* M, long long size); //打印单个单词的向量值
+
+int main(int argc, char** argv) {
+	FILE* f;
+	char st1[max_size];			//存储用户输入的字符串（单词或句子）
+	char* bestw[N];				//存储与输入单词最接近的前 N 个单词。
+	char file_name[max_size];	//存储词向量文件的路径。
+	char st[100][max_size];		//将用户输入的字符串拆分成单个单词存储
+	float dist;					//用于计算输入向量与其他向量之间的余弦相似度。
+	float len;					//向量的长度（模），用于归一化。
+	float bestd[N];				//存储与输入单词最接近的前 N 个单词的余弦相似度值。
+	float vec[max_size];		//用户输入单词向量的累积值。
+	long long words;			//文件中包含的单词总数。
+	long long size;				//每个单词向量的维度。
+	long long a, b, c, d;		//循环变量。
+	long long cn;				//输入的单词数
+	long long bi[100];			//输入单词在词汇表中的索引
+	float* M;					//存储所有词向量的矩阵（按行存储）
+	char* vocab;				//存储词汇表中所有单词。
+	if (argc < 2) {
+		printf("Usage: ./distance <FILE>\nwhere FILE contains word projections in the BINARY FORMAT\n");
+		return 0;
+	}
+	strcpy_s(file_name, sizeof(file_name), argv[1]);// C:/code/word2vec/vectors.bin word2vec程序训练出来的二进制数据文件。
+	errno_t err = fopen_s(&f, file_name, "rb");
+	if (f == NULL || 0 != err) {
+		printf("Input file not found\n");
+		return -1;
+	}
+	fscanf_s(f, "%lld", &words);	//71291 (用字符串存储，后面是一个空格0x20)
+	fscanf_s(f, "%lld", &size);	//200 (用字符串存储，后面是一个0x0A==\n==换行)
+	vocab = (char*)malloc((long long)words * max_w * sizeof(char));//单词表，每个单词最长50
+	for (a = 0; a < N; a++) bestw[a] = (char*)malloc(max_size * sizeof(char));//相似度最高的词条或句子
+	M = (float*)malloc((long long)words * (long long)size * sizeof(float));//每个单词用200个32位浮点数存储
+	if (M == NULL) {
+		printf("Cannot allocate memory: %lld MB    %lld  %lld\n", (long long)words * size * sizeof(float) / 1048576, words, size);
+		return -1;
+	}
+	for (b = 0; b < words; b++) {//71291个单词循环
+		a = 0;
+		while (1) {//读一个单词
+			vocab[b * max_w + a] = (char)fgetc(f);//读一个字符
+			if (feof(f) || (vocab[b * max_w + a] == ' ')) break;//空格或文件结束
+			if ((a < max_w) && (vocab[b * max_w + a] != '\n')) a++;
+		}
+		vocab[b * max_w + a] = 0;//单词结束符
+		for (a = 0; a < size; a++) fread(&M[a + b * size], sizeof(float), 1, f);//读取一个单词的200个float
+		len = 0;
+		if (0 == b)print_word_vec(&vocab[b * max_w], &M[b * size], size);//输出测试
+		for (a = 0; a < size; a++) len += M[a + b * size] * M[a + b * size];//向量平方和
+		len = (float)sqrt(len);
+		for (a = 0; a < size; a++) M[a + b * size] /= len;//归一化向量？
+		if (0 == b)print_word_vec(&vocab[b * max_w], &M[b * size], size);//输出测试
+	}
+	fclose(f);
+	while (1) {
+		for (a = 0; a < N; a++) bestd[a] = 0;
+		for (a = 0; a < N; a++) bestw[a][0] = 0;
+		printf("Enter word or sentence (EXIT to break): ");
+		a = 0;
+		while (1) {//读取用户输入的单词或句子，例如 france
+			st1[a] = (char)fgetc(stdin);
+			if ((st1[a] == '\n') || (a >= max_size - 1)) {
+				st1[a] = 0;
+				break;
+			}
+			a++;
+		}
+		if (!strcmp(st1, "EXIT")) break;
+		cn = 0;
+		b = 0;
+		c = 0;
+		while (1) {
+			st[cn][b] = st1[c];
+			b++;
+			c++;
+			st[cn][b] = 0;
+			if (st1[c] == 0) break;
+			if (st1[c] == ' ') {//输入的是句子，拆为多个单词存放
+				cn++;
+				b = 0;
+				c++;
+			}
+		}
+		cn++;
+		for (a = 0; a < cn; a++) {
+			for (b = 0; b < words; b++) if (!strcmp(&vocab[b * max_w], st[a])) break;
+			if (b == words) b = -1;
+			bi[a] = b;//找到每个单词的序号，例如 france 对应 vocab[303]
+			printf("\nWord: %s  Position in vocabulary: %lld\n", st[a], bi[a]);
+			if (b == -1) {
+				printf("Out of dictionary word!\n");
+				break;
+			}
+		}
+		if (b == -1) continue;
+		printf("\n                                              Word       Cosine distance\n------------------------------------------------------------------------\n");
+		for (a = 0; a < size; a++) vec[a] = 0;//单词向量累计值
+		for (b = 0; b < cn; b++) {
+			if (bi[b] == -1) continue;
+			for (a = 0; a < size; a++) vec[a] += M[a + bi[b] * size];
+		}
+		len = 0;
+		for (a = 0; a < size; a++) len += vec[a] * vec[a];
+		len = (float)sqrt(len);
+		for (a = 0; a < size; a++) vec[a] /= len;
+		for (a = 0; a < N; a++) bestd[a] = -1;//初始化距离
+		for (a = 0; a < N; a++) bestw[a][0] = 0;//初始化字符串
+		for (c = 0; c < words; c++) {//与所有单词计算距离
+			a = 0;
+			for (b = 0; b < cn; b++) if (bi[b] == c) a = 1;
+			if (a == 1) continue;
+			dist = 0;
+			for (a = 0; a < size; a++) dist += vec[a] * M[a + c * size];//距离计算
+			for (a = 0; a < N; a++) {
+				if (dist > bestd[a]) {//插入找到的距离更近的单词
+					for (d = N - 1; d > a; d--) {//移动后面的单词
+						bestd[d] = bestd[d - 1];
+						strcpy_s(bestw[d], max_size, bestw[d - 1]);
+					}
+					bestd[a] = dist;
+					strcpy_s(bestw[a], max_size, &vocab[c * max_w]);
+					break;
+				}
+			}
+		}
+		for (a = 0; a < N; a++) printf("%50s\t\t%f\n", bestw[a], bestd[a]);
+	}
+	return 0;
+}
+
+void print_word_vec(char* word_str, float* M, long long size) {
+	//打印一个单词的向量，典型值就是100个浮点数
+	const int count_per_line = 10;//每行的浮点数
+	const int line_count = (int)(size / count_per_line);//完整的行数
+	const int less_count = size % count_per_line;//最后一行的数目
+	printf("%s\n", NULL == word_str ? "NULL" : word_str);
+	for (int line_index = 0; line_index < line_count; ++line_index) {
+		for (int data_index = 0; data_index < count_per_line; ++data_index) {
+			printf("%10.6f ", M[line_index * count_per_line + data_index]);
+		}
+		printf("\n");
+	}
+	if (less_count > 0) {
+		for (int data_index = 0; data_index < less_count; ++data_index) {
+			printf("%10.6f ", M[line_count * count_per_line + data_index]);
+		}
+		printf("\n");
+	}
 }
